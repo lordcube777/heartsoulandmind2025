a3b8b3f5acc1ee6a5e652db8ff54192764e7849b
lordcube777 <91109380+lordcube777@users.noreply.github.com>
2025-09-18 11:30:00 -0400

    updated version of resize

--- inc/hsm_image_resize.php
+++ inc/hsm_image_resize.php
@@ -8,23 +8,26 @@ function upload_overrides( $overrides, $file ) {
 }
 function image_filename_override( $dir, $name, $ext ) {
     preg_match('/^(.*)-?(verse|quote|carousel|reel)?-?(light|dark)?-?(landscape|square|standard|portrait-short|portrait-tall).[a-z0-9]+$/U', $name, $name_parts);
-    if ( $name_parts[4] == 'landscape' ) {
-        $name = pathinfo( $name, PATHINFO_FILENAME ) . "-1280w" . $ext;
-    }
+    // if ( $name_parts[4] == 'landscape' ) {
+    //     $name = pathinfo( $name, PATHINFO_FILENAME ) . "-1280w" . $ext;
+    // }
     $unique_name = "$name_parts[1]/$name";
     return $unique_name;
 }
-add_filter( 'pre_move_uploaded_file', 'hsm_image_resize', 10, 4 );
-function hsm_image_resize( $move_new_file, $file, $new_file, $type ) {
-    $move_new_file = false;
+add_filter( 'pre_move_uploaded_file', 'create_upload_dir', 10, 3 );
+function create_upload_dir( $move_new_file, $file, $new_file ) {
     if ( ! is_writable( dirname( $new_file ) ) ) {
         mkdir( dirname( $new_file ), 0755 );
     }
-    if ( preg_match( '#^image#', $file['type'] ) ) {
+}
+add_filter( 'wp_handle_upload', 'hsm_image_resize', 10, 2 );
+function hsm_image_resize( $upload, $context ) {
+    $featured_image = false;
+    if ( preg_match( '#^image#', $upload['type'] ) ) {
         $sizes = array(128, 256, 512, 768, 1024, 1536, 2048);
-        $filename = pathinfo( $file['name'], PATHINFO_FILENAME );
-        $dirname = pathinfo( $new_file, PATHINFO_DIRNAME );
-        $image = new Imagick($file['tmp_name']);
+        $filename = pathinfo( $upload['file'], PATHINFO_FILENAME );
+        $dirname = pathinfo( $upload['file'], PATHINFO_DIRNAME );
+        $image = new Imagick($upload['file']);
         if ( $image->getImageAlphaChannel() ) {
             $extensions = array( 'png', 'webp' );
         } else {
@@ -54,20 +57,20 @@ function hsm_image_resize( $move_new_file, $file, $new_file, $type ) {
                 $image->stripImage();
                 $image->writeImage( $output_filename );
             }
-            if ( preg_match('/^.*-1280w.[a-z0-9]+$/', $new_file ) && $extension == 'jpg' || $extension == 'png' ) {
-                $output_filename = $new_file;
+            preg_match('/^(.*)-?(verse|quote|carousel|reel)?-?(light|dark)?-?(landscape|square|standard|portrait-short|portrait-tall).[a-z0-9]+$/U', $name, $name_parts);
+            if ( $name_parts[4] == 'landscape' && $extension == 'jpg' || $extension == 'png' ) {
+                 $output_filename = $dirname . "/" . $filename . "-1280w." . $extension;
                 $image->scaleImage( 1280, 720, true );
                 $image->stripImage();
                 $image->writeImage( $output_filename );
-                //$move_new_file = null;
+                $featured_image = true;
             }
         }
         $image->clear();
-        if ( ! $move_new_file ) {
-            unlink( $file['tmp_name'] );
+        if ( ! $featured_image ) {
+            unlink( $upload['file'] );
+            $upload['error'] = "The file was sucsessfully upload, resized, and deleted.";
         }
-        return $move_new_file;
-    } else {
-        return null;
     }
+    return $upload;
 }
